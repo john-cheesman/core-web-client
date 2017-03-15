df72f36776f63303a09d8cc4d351ab38eccd1fdd
John Cheesman <me@johncheesman.org.uk>
2017-03-15 15:23:37 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="app">
+        <hello></hello>
+    </div>
+</template>
+
+<script>
+    import Hello from './components/Hello.vue'
+
+    export default {
+        name: 'app',
+        components: {
+            Hello
+        },
+        created() {
+            this.getSession()
+        },
+        methods: {
+            getSession() {
+                this.$http.get('http://core-web-api.herokuapp.com/api/session')
+                    .then(
+                        (response) => {
+                            console.log(response)
+                        },
+                        (error) => {
+                            console.warn(error)
+                        }
+                    )
+            }
+        }
+    }
+</script>
---
+++ src/components/Hello.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="hello">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'hello',
+        data() {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
+
